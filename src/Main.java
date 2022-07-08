5ff3e373c1b45a51c37770033a396f296c951676
EshtaJee <Logrus-10@yandex.ru>
2022-07-08 21:00:59 +0300

    FixPayment

--- src/Main.java
+++ src/Main.java
@@ -11,19 +11,19 @@ public class Main {
         String annuityPay = String.format("%.0f",payment); // аннуитетный платеж (окр.)
         System.out.println();
         System.out.println("Хочешь взять " + creditAmount + " руб. На " + creditTerm + " год(а) по кредитной ставке " + interestRate + " % годовых?!");
-        System.out.println("Тогда каждый месяц будешь заносить по " + annuityPay + " руб. Иначе... Ну ты сам знаешь...");
+        System.out.println("Тогда каждый месяц будешь заносить по " + annuityPay + " руб.");
 
         double payment1 = service.calculate(creditAmount, (creditTerm+1), interestRate); // аннуитетный платеж
         String annuityPay1 = String.format("%.0f",payment1); // аннуитетный платеж (окр.)
         System.out.println();
         System.out.println("Хочешь взять " + creditAmount + " руб. На " + (creditTerm+1) + " год(а) по кредитной ставке " + interestRate + " % годовых?!");
-        System.out.println("Тогда каждый месяц будешь заносить по " + annuityPay1 + " руб. Иначе... Ну ты сам знаешь...");
+        System.out.println("Тогда каждый месяц будешь заносить по " + annuityPay1 + " руб.");
 
         double payment2 = service.calculate(creditAmount, (creditTerm+2), interestRate); // аннуитетный платеж
         String annuityPay2 = String.format("%.0f",payment2); // аннуитетный платеж (окр.)
         System.out.println();
         System.out.println("Хочешь взять " + creditAmount + " руб. На " + (creditTerm+2) + " год(а) по кредитной ставке " + interestRate + " % годовых?!");
-        System.out.println("Тогда каждый месяц будешь заносить по " + annuityPay2 + " руб. Иначе... Ну ты сам знаешь...");
+        System.out.println("Тогда каждый месяц будешь заносить по " + annuityPay2 + " руб.");
 
     }
 
