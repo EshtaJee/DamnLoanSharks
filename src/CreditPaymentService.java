5fc79adedbb95d3e320a59d33c267c5cd0d15f14
EshtaJee <Logrus-10@yandex.ru>
2022-07-08 20:39:24 +0300

    Payment

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class CreditPaymentService {
+public class CreditPaymentService {
+    public double calculate(int creditAmount, int creditTerm, double interestRate) {
+
+        // Аннуитетный платёж рассчитывается по формуле: Сумма кредита × Коэффициент аннуитета
+        //Коэффициент рассчитывается следующим образом:
+        // (m × (1 + m)ⁿ) / ((1 + m)ⁿ — 1)
+        // где m — процентная ставка в месяц, равная годовой ставке, поделённой на 12
+        // n — количество платежей
+
+        int n = (creditTerm * 12); // Срок кредита (мес)
+        double m = (interestRate / 12) /100; // процентная ставка (мес)
+        double exponent = Math.pow((1 + m), n);
+        double coefficient = (m * exponent) / (exponent - 1); // итоговый коэффициент
+        return (creditAmount * coefficient); // аннуитетный платеж
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    public static void main(String[] args) {
+        CreditPaymentService service = new CreditPaymentService();
+
+        int creditAmount = 1000000; // сумма кредита
+        int creditTerm = 1; // срок кредита (год)
+        double interestRate = 9.99; // годовая процентная ставка
+
+        double payment = service.calculate(creditAmount, creditTerm, interestRate); // аннуитетный платеж
+        String annuityPay = String.format("%.0f",payment); // аннуитетный платеж (окр.)
+        System.out.println();
+        System.out.println("Хочешь взять " + creditAmount + " руб. На " + creditTerm + " год(а) по кредитной ставке " + interestRate + " % годовых?!");
+        System.out.println("Тогда каждый месяц будешь заносить по " + annuityPay + " руб. Иначе... Ну ты сам знаешь...");
+
+        double payment1 = service.calculate(creditAmount, (creditTerm+1), interestRate); // аннуитетный платеж
+        String annuityPay1 = String.format("%.0f",payment1); // аннуитетный платеж (окр.)
+        System.out.println();
+        System.out.println("Хочешь взять " + creditAmount + " руб. На " + (creditTerm+1) + " год(а) по кредитной ставке " + interestRate + " % годовых?!");
+        System.out.println("Тогда каждый месяц будешь заносить по " + annuityPay1 + " руб. Иначе... Ну ты сам знаешь...");
+
+        double payment2 = service.calculate(creditAmount, (creditTerm+2), interestRate); // аннуитетный платеж
+        String annuityPay2 = String.format("%.0f",payment2); // аннуитетный платеж (окр.)
+        System.out.println();
+        System.out.println("Хочешь взять " + creditAmount + " руб. На " + (creditTerm+2) + " год(а) по кредитной ставке " + interestRate + " % годовых?!");
+        System.out.println("Тогда каждый месяц будешь заносить по " + annuityPay2 + " руб. Иначе... Ну ты сам знаешь...");
+
+    }
+
 }
